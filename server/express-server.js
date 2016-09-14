74481e93ad20175d1cbe5b376d931a18ba7547c0
Sabrina Ooi <heysabs@gmail.com>
2016-09-14 15:02:58 +0800

    fixed express server static route again

--- server/express-server.js
+++ server/express-server.js
@@ -3,6 +3,6 @@ var app     = express();
 
 //For avoidong Heroku $PORT error
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '../public'))
 
 app.listen('port', (process.env.PORT || 5000))
