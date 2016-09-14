f4c11f8f6c53b5e7a2adc47cf871525557f48b14
Sabrina Ooi <heysabs@gmail.com>
2016-09-14 15:43:37 +0800

    update express server file

--- server/express-server.js
+++ server/express-server.js
@@ -3,6 +3,6 @@ var app     = express();
 
 //For avoidong Heroku $PORT error
 
-app.use(express.static(__dirname + '../public'))
+app.use('/', express.static('./public'));
 
 app.listen(process.env.PORT || 5000)
